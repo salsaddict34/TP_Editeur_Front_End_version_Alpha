7638f851aa25e242a527a8ba5421015951f7e0ca
salsaddict34 <salsaddict34@free.fr>
2022-09-30 18:30:18 +0200

    Ajout scrollbar

--- js/udapteIframe.js
+++ js/udapteIframe.js
@@ -2,13 +2,14 @@
 /* update the iframe view whenever HTML, CSS, or JS/jQuery is enter */
 /*------------------------------------------------------------------*/
 
-function updateIframe(){
+function updateIframe() {
     var textareaHtmlValue = $(".htmlArea__text").val();
     var textareaCssValue = $(".cssArea__text").val();
     var textareaJsValue = $(".jsArea__text").val();
+    textareaJsValue = textareaJsValue.replaceAll(/\$/g, "parent.$");
+    textareaJsValue = textareaJsValue.replaceAll(/\)\./g, ", document).");
     var cssLink = "<style>" + textareaCssValue + "</style>";
-    var js ='<script>'+textareaJsValue+'<\/script>';
-    var head_jquery = '<script src="https://code.jquery.com/jquery-3.6.1.min.js"><\/script>';
+    var js = '<script>' + textareaJsValue + '<\/script>';
     var myFrame = $(".displayArea__result").contents().find('body');
     var head = $(".displayArea__result").contents().find("head");
     head.empty();
@@ -20,50 +21,50 @@ function updateIframe(){
     textHtml = textHtml.replaceAll(/<[^>]+>|$nbsp;| |\n/g, '');
     var countHtmlChar = textHtml.length;
     var htmlMaxLength = $('.htmlArea__text').attr('maxlength');
-    var rateHtml = (countHtmlChar/htmlMaxLength)*100;
-    $( ".htmlArea__progressbar" ).progressbar( "value", countHtmlChar );
-    $( ".htmlArea__progressbar" ).progressbar( "option", "max", htmlMaxLength);
-    if ( rateHtml < 25) {
-        $('.htmlArea__progressbar > div').css({'background': 'green'});
+    var rateHtml = (countHtmlChar / htmlMaxLength) * 100;
+    $(".htmlArea__progressbar").progressbar("value", countHtmlChar);
+    $(".htmlArea__progressbar").progressbar("option", "max", htmlMaxLength);
+    if (rateHtml < 25) {
+        $('.htmlArea__progressbar > div').css({ 'background': 'green' });
     } else if (rateHtml > 25 && rateHtml < 50) {
-        $('.htmlArea__progressbar > div').css({'background': 'yellow'});
+        $('.htmlArea__progressbar > div').css({ 'background': 'yellow' });
     } else if (rateHtml > 50 && rateHtml < 75) {
-        $('.htmlArea__progressbar > div').css({'background': 'orange'});
+        $('.htmlArea__progressbar > div').css({ 'background': 'orange' });
     } else if (rateHtml > 75) {
-        $('.htmlArea__progressbar > div').css({'background': 'red'});
+        $('.htmlArea__progressbar > div').css({ 'background': 'red' });
     }
     /******************************CSS****************************************/
     var textCss = $('.cssArea__text').val();
     textCss = textCss.replaceAll(/\{|\}|\s|:|;|\.|#|\n/g, '');
     var countCssChar = textCss.length
     var cssMaxLength = $('.cssArea__text').attr('maxlength');
-    var rateCss = (countCssChar/cssMaxLength)*100;
-    $( ".cssArea__progressbar" ).progressbar( "value", countCssChar );
-    $( ".cssArea__progressbar" ).progressbar( "option", "max", cssMaxLength);
-    if ( rateCss < 25) {
-        $('.cssArea__progressbar > div').css({'background': 'green'});
+    var rateCss = (countCssChar / cssMaxLength) * 100;
+    $(".cssArea__progressbar").progressbar("value", countCssChar);
+    $(".cssArea__progressbar").progressbar("option", "max", cssMaxLength);
+    if (rateCss < 25) {
+        $('.cssArea__progressbar > div').css({ 'background': 'green' });
     } else if (rateCss > 25 && rateCss < 50) {
-        $('.cssArea__progressbar > div').css({'background': 'yellow'});
+        $('.cssArea__progressbar > div').css({ 'background': 'yellow' });
     } else if (rateCss > 50 && rateCss < 75) {
-        $('.cssArea__progressbar > div').css({'background': 'orange'});
+        $('.cssArea__progressbar > div').css({ 'background': 'orange' });
     } else if (rateCss > 75) {
-        $('.cssArea__progressbar > div').css({'background': 'red'});
+        $('.cssArea__progressbar > div').css({ 'background': 'red' });
     }
     /******************************JS****************************************/
     var textJs = $('.jsArea__text').val();
     var countJsChar = textJs.length;
     var jsMaxLength = $('.jsArea__text').attr('maxlength');
-    var rateJs = (countJsChar/jsMaxLength)*100;
-    $( ".jsArea__progressbar" ).progressbar( "value", countJsChar );
-    $( ".jsArea__progressbar" ).progressbar( "option", "max", jsMaxLength);
-    if ( rateJs < 25) {
-        $('.jsArea__progressbar > div').css({'background': 'green'});
+    var rateJs = (countJsChar / jsMaxLength) * 100;
+    $(".jsArea__progressbar").progressbar("value", countJsChar);
+    $(".jsArea__progressbar").progressbar("option", "max", jsMaxLength);
+    if (rateJs < 25) {
+        $('.jsArea__progressbar > div').css({ 'background': 'green' });
     } else if (rateJs > 25 && rateJs < 50) {
-        $('.jsArea__progressbar > div').css({'background': 'yellow'});
+        $('.jsArea__progressbar > div').css({ 'background': 'yellow' });
     } else if (rateJs > 50 && rateJs < 75) {
-        $('.jsArea__progressbar > div').css({'background': 'orange'});
+        $('.jsArea__progressbar > div').css({ 'background': 'orange' });
     } else if (rateJs > 75) {
-        $('.jsArea__progressbar > div').css({'background': 'red'});
+        $('.jsArea__progressbar > div').css({ 'background': 'red' });
     }
     /*----------------------------------------------------------*/
     event.preventDefault();
